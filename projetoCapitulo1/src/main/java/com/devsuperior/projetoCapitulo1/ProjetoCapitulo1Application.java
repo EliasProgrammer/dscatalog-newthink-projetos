daff6f0daaad90fc57298f2dfa6781f4e0f9e0f3
Elias Mendes <elias.mendes.2009@gmail.com>
2021-12-20 22:56:34 -0300

    Chapter 1 project created

---
+++ projetoCapitulo1/src/main/java/com/devsuperior/projetoCapitulo1/ProjetoCapitulo1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projetoCapitulo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCapitulo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCapitulo1Application.class, args);
+	}
+
+}
---
+++ projetoCapitulo1/src/test/java/com/devsuperior/projetoCapitulo1/ProjetoCapitulo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projetoCapitulo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCapitulo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
